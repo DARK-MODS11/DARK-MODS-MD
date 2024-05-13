2732f95d5dc027891c297df9d95688a488d25ea9
Abhinandh <104196277+DARK-MODS11@users.noreply.github.com>
2024-05-13 07:40:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,45 +1,30 @@
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
-require("dotenv").config();
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
 
-const toBool = (x) => x === "true";
+const toBool = (x) => x == "true";
 
-const DATABASE_URL = process.env.DATABASE_URL || "./assets/database.db";
-
+DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
+let HANDLER = "false";
 module.exports = {
+  SPARKY_API: "https://api-aswin-sparky.koyeb.app",
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID: process.env.SESSION_ID || "idvu3oj6_XASENA_w46_XASENA_nb8c",
+  SESSION_ID:process.env.SESSION_ID || " ",
   LANG: process.env.LANG || "EN",
-  AUTH_TOKEN: "",
-  HANDLERS:
-    process.env.HANDLER === "false" || process.env.HANDLER === "null"
-      ? "^"
-      : "^[#]",
-  RMBG_KEY: process.env.RMBG_KEY || false,
-  BRANCH: "main",
-  WARN_COUNT: 3,
-  PACKNAME: process.env.PACKNAME || "X-asena",
-  WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
-  GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
-  AUTHOR: process.env.AUTHOR || "X-electra",
-  SUDO:
-    process.env.SUDO || "918113921898,919598157259,918590508376,919383400679",
-  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
-  OWNER_NAME: process.env.OWNER_NAME || "Neeraj-X0",
-  HEROKU: toBool(process.env.HEROKU) || false,
-  BOT_NAME: process.env.BOT_NAME || "X-asena",
-  PROCESSNAME: process.env.PROCESSNAME || "x-asena",
+  HANDLERS: process.env.PREFIX || '^[.]',
+  BRANCH: "master",
+  STICKER_DATA: process.env.STICKER_DATA || "Dɪyᴀʜ🧚‍♀️;nazim.x10",
+  BOT_INFO: process.env.BOT_INFO || "Abhinandh;DARK-MODS-MD;https://i.imgur.com/nAkM1zV.mp4",
+  AUDIO_DATA: process.env.AUDIO_DATA || "DARK-MODS-MD;Abhinandh;https://i.imgur.com/Ou56ggv.jpeg",
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+  CAPTION: process.env.CAPTION || "DARK-MODS-MD!",
   WORK_TYPE: process.env.WORK_TYPE || "private",
-  SESSION_URL: process.env.SESSION_URL || "",
-  DELETED_LOG: toBool(process.env.DELETED_LOG) || false,
-  DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
   DATABASE_URL: DATABASE_URL,
-  STATUS_SAVER: toBool(process.env.STATUS_SAVER) || true,
   DATABASE:
-    DATABASE_URL === "./assets/database.db"
+    DATABASE_URL === "./lib/database.db"
       ? new Sequelize({
           dialect: "sqlite",
           storage: DATABASE_URL,
@@ -55,4 +40,8 @@ module.exports = {
           },
           logging: false,
         }),
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
+  SUDO: process.env.SUDO || "918891871875", "919747257996",
+  IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
 };
