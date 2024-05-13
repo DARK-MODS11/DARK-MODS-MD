b12c7016790bc069e551879d5f06a797d6087e0d
Abhinandh <104196277+DARK-MODS11@users.noreply.github.com>
2024-05-13 07:45:56 +0530

    Update ping.js

--- assets/plugins/ping.js
+++ assets/plugins/ping.js
@@ -1,20 +1,21 @@
 const { fromBuffer, mimeTypes } = require("file-type");
 const { command, isPrivate } = require("../../lib/");
 command(
-  {
-    pattern: "ping",
-    fromMe: isPrivate,
-    desc: "To check ping",
-    type: "user",
-  },
-  async (message, match) => {
-    const start = new Date().getTime();
-    await message.sendMessage(message.jid, "```Ping!```");
-    const end = new Date().getTime();
-    return await message.sendMessage(
-      message.jid,
-      "*Pong!*\n ```" + (end - start) + "``` *ms*"
-    );
-  }
-);
+    {
+        pattern: "ping",
+        fromMe: isPrivate,
+        desc: "To check ping",
+        type: "user",
+    },
+    async (message, match, client) => {
+        const start = new Date().getTime();
+      let { key } = await message.sendMessage(`*𝙿𝚒𝚗𝚐 🧃*`);
+        const end = new Date().getTime();
+var speed = end - start;
+ 
+await new Promise(t => setTimeout(t,0))
+         await message.client.sendMessage(message.jid,{text:`*𝙿𝚘𝚗𝚐* 🧃
+${speed} *𝚖𝚜*` , edit: key});
+})
+
 
